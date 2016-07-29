81a2c8fc05c9bbad6296a5ddf6be393c354cce83
Sergei Zelinsky <sergei.zelinsky.dev@gmail.com>
2016-07-29 19:52:36 +0300

    wrapped App component in ShutterDGGApp

--- index.android.js
+++ index.android.js
@@ -1,6 +1,15 @@
+import React, { Component } from 'react';
 import {
   AppRegistry
 } from 'react-native';
 import App from './src/App.js';
 
-AppRegistry.registerComponent('ShutterDGG', () => App);
+class ShutterDGGApp extends Component {
+  render(){
+    return (
+      <App/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('ShutterDGG', () => ShutterDGGApp);
