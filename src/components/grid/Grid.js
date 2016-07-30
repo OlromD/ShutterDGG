aabe195a51ec9d8a6947084b450d4085e6dee388
Sergei Zelinsky <sergei.zelinsky.dev@gmail.com>
2016-07-30 22:58:09 +0300

    rewrite Grid component

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -3,39 +3,40 @@ import {
   View,
   Text
 } from 'react-native';
-
-import styles from '../../style/GlassGridStyle';
-import GridRow from './GridRow';
-import GridCell from './GridCell';
 import Canvas from '../Canvas';
+import styles from '../../style/GlassGridStyle';
 
+// function which will be passed to the WebView as a string
 function renderCanvas(canvas){
-  const ctx = canvas.getContext('2d');
-  const that = this;
+  const ctx = canvas.getContext('2d'),
+        self = this;
   function draw(){
-    const {hIndicators, vIndicators, size, rows, cols} = that;
-    const w = cols * size;
-    const h = rows*size;
+    const { hIndicators, vIndicators, size, rows, cols } = self,
+          w = cols * size,
+          h = rows * size,
+          ACTIVE_COLOR = 'rgb(255, 255, 255)',
+          NO_ACTIVE_COLOR = 'rgb(160,160,160)',
+          GRID_LINE_COLOR = 'rgb(180,180,180)',
+          GRID_LINE_WIDTH = 1;
     canvas.width = w;
     canvas.height = h;
     ctx.clearRect(0, 0, w, h);
-    for (var i = 0; i < cols; i++){
-      for (var j = 0; j < rows; j++){
-        // ctx.fillStyle = (hIndicators[i] !== vIndicators[j])?'#FFFFFF': '#000000';
-        ctx.fillStyle = that.hIndicators[i] === that.vIndicators[j]?'rgb(160,160,160)': 'white';
+    for (let i = 0; i < cols; i++){
+      for (let j = 0; j < rows; j++){
+        ctx.fillStyle = ( self.hIndicators[i] === self.vIndicators[j] )? NO_ACTIVE_COLOR : ACTIVE_COLOR;
         ctx.fillRect(i * size, j * size, (i + 1) * size, (j + 1) * size);
       }
     }
-    ctx.lineWidth = 1;
+    ctx.lineWidth = GRID_LINE_WIDTH;
     for (let i = 0; i < rows; i++){
-      ctx.moveTo(0, i*size);
-      ctx.lineTo(w, i*size);
+      ctx.moveTo(0, i * size);
+      ctx.lineTo(w, i * size);
     }
     for (let i = 0; i < cols; i++){
-      ctx.moveTo(i*size, 0);
-      ctx.lineTo(i*size, h);
+      ctx.moveTo(i * size, 0);
+      ctx.lineTo(i * size, h);
     }
-    ctx.strokeStyle = 'rgb(180,180,180)';
+    ctx.strokeStyle = GRID_LINE_COLOR;
     ctx.stroke();
   }
   draw();
@@ -47,37 +48,31 @@ export default class Grid extends Component{
     super(props);
   }
 
-
-
-
   render(){
-    const {cellSize, verticalIndicators, horizontalIndicators} = this.props;
-    const w = cellSize*horizontalIndicators.length,
-          h = cellSize*verticalIndicators.length;
+    const { cellSize, verticalIndicators, horizontalIndicators } = this.props,
+          w = cellSize * horizontalIndicators.length,
+          h = cellSize * verticalIndicators.length,
+          context = {
+            hIndicators: horizontalIndicators,
+            vIndicators: verticalIndicators,
+            size:        cellSize,
+            rows:        verticalIndicators.length,
+            cols:        horizontalIndicators.length
+          },
+          webViewStyles = {
+            width:            w,
+            height:           h,
+            backgroundColor:  'rgb(160,160,160)'
+          };
+
     return (
-      <View style={styles.gridCellsContainer}>
-      <Canvas
-        context={{hIndicators: horizontalIndicators, vIndicators : verticalIndicators, size : cellSize, rows : verticalIndicators.length, cols : horizontalIndicators.length}}
-        render={renderCanvas}
-        style={{width: w, height: h, backgroundColor: 'rgb(160,160,160)'}}
-      />
+      <View style = { styles.gridCellsContainer }>
+        <Canvas
+          context = { context }
+          render = { renderCanvas }
+          style = { webViewStyles }
+        />
       </View>
     );
-  }
-
-  _getGridRow(row){
-    let res = [];
-    const { cellSize, horizontalIndicators, verticalIndicators} = this.props;
-    for (let i = 0; i < horizontalIndicators.length; i++)
-      res.push(<GridCell key={i} isActive={horizontalIndicators[i] !== verticalIndicators[row]} size={cellSize}/>);
-    return res;
-  }
-  _getGrid(){
-    const {cellSize, verticalIndicators, horizontalIndicators} = this.props;
-    let res = [];
-    for (let i = 0; i < verticalIndicators.length; i++){
-      res.push(<GridRow key={i} value={verticalIndicators[i]} horizontalIndicators={horizontalIndicators}>{this._getGridRow(i)}</GridRow>);
-    }
-    return res;
   }
 }
