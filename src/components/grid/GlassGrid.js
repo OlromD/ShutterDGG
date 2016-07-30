1ea8f75be02351619737ab87e9f9ed92bf2becf9
Sergei Zelinsky <sergei.zelinsky.dev@gmail.com>
2016-07-30 23:13:38 +0300

    deleted unused components

--- src/components/grid/GlassGrid.js
+++ src/components/grid/GlassGrid.js
@@ -5,40 +5,38 @@ import {
 } from 'react-native';
 
 import styles from '../../style/GlassGridStyle';
-
-
 import HorizontalIndicators from './HorizontalIndicators';
 import VerticalIndicators from './VerticalIndicators';
 import Grid from './Grid';
 
-export default class GlassGrid extends Component{
+export default class GlassGrid extends Component {
   constructor(props){
     super(props);
   }
 
   render(){
-    const {rows, cols, cellSize, horizontalIndicators, verticalIndicators, horizontalIndex, verticalIndex} = this.props;
+    const { rows, cols, cellSize, horizontalIndicators, verticalIndicators, horizontalIndex, verticalIndex } = this.props;
     return (
-      <View style={styles.grid}>
-        <View style={styles.gridContainer}>
-          <View style={{flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+      <View style = { styles.grid }>
+        <View style = { styles.gridContainer }>
+          <View style = { { flexDirection: 'column', alignItems: 'center', justifyContent: 'center' } }>
             <HorizontalIndicators
-              cols={cols}
-              horizontalIndicators={horizontalIndicators}
-              cellSize={cellSize}
-              horizontalIndex={horizontalIndex}
+              cols = { cols }
+              horizontalIndicators = { horizontalIndicators }
+              cellSize = { cellSize }
+              horizontalIndex = { horizontalIndex }
             />
             <Grid
-              cellSize={cellSize}
-              verticalIndicators={verticalIndicators}
-              horizontalIndicators={horizontalIndicators}
+              cellSize = { cellSize }
+              verticalIndicators = { verticalIndicators }
+              horizontalIndicators = { horizontalIndicators }
             />
           </View>
           <VerticalIndicators
-            rows={rows}
-            cellSize={cellSize}
-            verticalIndex={verticalIndex}
-            verticalIndicators={verticalIndicators}
+            rows = { rows }
+            cellSize = { cellSize }
+            verticalIndex = { verticalIndex }
+            verticalIndicators = { verticalIndicators }
           />
         </View>
       </View>
