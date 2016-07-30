33affb9661056a421acda9f61fd4a1d6d352f517
Sergei Zelinsky <sergei.zelinsky.dev@gmail.com>
2016-07-30 23:06:24 +0300

    rewrote Canvas component

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -6,18 +6,25 @@ import {
 
 export default class Canvas extends Component{
   render() {
-    var contextString = JSON.stringify(this.props.context);
-    var renderString = this.props.render.toString();
+    const contextString = JSON.stringify(this.props.context),
+          renderString = this.props.render.toString(),
+          contentInset = {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+          },
+          htmlString = "<style>*{margin:0;padding:0;}canvas{position:absolute;transform:translateZ(0);}</style><canvas></canvas><script>var canvas = document.querySelector('canvas');(" + renderString + ").call(" + contextString + ", canvas);</script>";
     return (
       <View>
         <WebView
-          automaticallyAdjustContentInsets={false}
-          contentInset={{top: 0, right: 0, bottom: 0, left: 0}}
-          source={{html: "<style>*{margin:0;padding:0;}canvas{position:absolute;transform:translateZ(0);}</style><canvas></canvas><script>var canvas = document.querySelector('canvas');(" + renderString + ").call(" + contextString + ", canvas);</script>"}}
-          opaque={false}
-          javaScriptEnabled={true}
-          underlayColor={'transparent'}
-          style={this.props.style}
+          automaticallyAdjustContentInsets = { false }
+          contentInset = { contentInset }
+          source = { { html: htmlString } }
+          opaque = { false }
+          javaScriptEnabled = { true }
+          underlayColor = { 'transparent' }
+          style = { this.props.style }
         />
       </View>
     );
