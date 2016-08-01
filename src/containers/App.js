37e7dd73e32cb5ef90854b9e0581bb3071faba0e
Sergei Zelinsky <sergei.zelinsky.dev@gmail.com>
2016-08-01 23:44:31 +0300

    rewrote navigation config

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,6 +8,16 @@ import MainPage from '../components/MainPage';
 import DesignPage from '../components/DesignPage';
 import styles from '../style/ApplicationStyle.js';
 
+const navigationConfig = {
+  'MainPage': {
+    component: MainPage,
+  },
+  'DesignPage' : {
+    component : DesignPage
+  }
+}
+
+
 export default class App extends Component {
   componentDidMount(){
     this.setState({
@@ -25,29 +35,30 @@ export default class App extends Component {
     return (
           <View style={styles.applicationContainer}>
             <Navigator
-              initialRoute={{id : 'MainPage', name : 'Index'}}
-              renderScene={this.renderScene.bind(this)}
-              configureScene={this.configureScene.bind(this)}
+              initialRoute={ { id : 'MainPage' } }
+              renderScene={this.navigatorRenderScene.bind(this)}
+              configureScene={this.navigatorConfigureScene.bind(this)}
             />
           </View>
     );
   }
 
-  configureScene(route){
+  navigatorConfigureScene(route){
     if (route.sceneConfig){
       return route.sceneConfig
     }
     return Navigator.SceneConfigs.FloatFromBottomAndroid;
   }
 
-  renderScene(route, navigator){
-    const routeID = route.id;
-    if (routeID === 'MainPage'){
-      return <MainPage setDimensions={this._setDimensions.bind(this)} navigator={navigator}/>
-    }
-    if (routeID === 'DesignPage'){
-      return <DesignPage width={this.state.width} height={this.state.height} navigator={navigator}/>
-      // return <DesignPage width={180} height={360} navigator={navigator}/>
-    }
+  navigatorRenderScene(route, navigator){
+    const routeID = route.id,
+          Component = navigationConfig[routeID];
+
+    return (<Component
+                setDimensions = { this._setDimensions.bind(this) }
+                navigator = { navigator }
+                width = { this.state.width }
+                height = { this.state.height }
+            />);
   }
 }
